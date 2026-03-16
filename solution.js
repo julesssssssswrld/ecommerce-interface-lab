b9ae25586feb76e6f39a4975d3b99fb65dd93f52
japcakesssxyz <jovanatencio17@gmail.com>
2026-03-16 10:34:32 +0800

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,12 @@
+class Item {
+    #discount = 0.1; // The # makes it private
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    get finalPrice() {
+        return this.price - (this.price * this.#discount);
+    }
+}
